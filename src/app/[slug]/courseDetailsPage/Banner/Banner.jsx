14ab4030809253e2b3ca4987ce3547d579922b0e
medamvishnu27 <medamvishnu27@gmail.com>
2025-10-01 19:45:34 +0530

    updated the banner text  & ssstories month

--- src/app/[slug]/courseDetailsPage/Banner/Banner.jsx
+++ src/app/[slug]/courseDetailsPage/Banner/Banner.jsx
@@ -40,14 +40,16 @@ const Banner = () => {
           </div>
           <div className={`${style.bannerPrice}`}>
             <span className={style.originalPrice}>
-              #LearnTillYou<h1 className={style.offerText1}>GetPlaced</h1> 
-              {/* <p>
-                Almost Full{" "}
-                <span className={style.offerText}>Only 5 Slots Left!</span>
-              </p> */}
+              <span className={style.whiteBold}>Earn While Learning</span>
+              <br />
+              <span className={style.grayText}>(upto</span>
+              <span className={style.redHighlight}> 45,000 INR - </span>
+             
+              <br className={style.smallBr} />
+              <span className={style.grayText}>stipend based intenships)</span>
             </span>
           </div>
-          <div >
+          <div className={style.enrollButtonContainer}>
             {card && (
               <EnrollButton
                 courseID={card.id}
@@ -56,7 +58,6 @@ const Banner = () => {
                 label={
                   <>
                     <FontAwesomeIcon icon={faDownload} />
-                    
                      &nbsp;Get Details
                   </>
                 }
