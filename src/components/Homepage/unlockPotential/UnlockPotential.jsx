ffc64e53229867737861b5a30a31caf661b4fc17
Vishnu Vardhan Reddy <162047840+medamvishnu27@users.noreply.github.com>
2025-12-04 13:14:29 +0530

    Refactor UnlockPotential component layout
    Removed heading elements and commented out image in UnlockPotential component.

--- src/components/Homepage/unlockPotential/UnlockPotential.jsx
+++ src/components/Homepage/unlockPotential/UnlockPotential.jsx
@@ -70,17 +70,17 @@ const UnlockPotential = () => {
                   Your Journey To a Dream Job Starts Here
                 </p>
               </div>
-              <h1 className={`${homeStyle.title}`}>
-                Hyderabad’s No.1 Training Institute for Data Science, AI, Full Stack & Cloud
-              </h1>
-              <h2>
-                Job-Ready Courses with Real Internships, Stipends & 100% Placement Support
-              </h2>
-              {/* <Image
+              <p className={`${homeStyle.title}`}>
+                Unlock Your Potential with SocialPrachar
+              </p>
+              <Image
+
+
+
                 src={yellowLine}
                 alt="yellowLine"
                 className={`${homeStyle.yellowLine} `}
-              /> */}
+              />
               <br />
               <p className="greyText ">
                 Discover a world of knowledge and opportunities with our Mentor
