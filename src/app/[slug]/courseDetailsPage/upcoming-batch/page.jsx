82d8904f12d5049796033b05a19be070e43420a3
Vishnu Vardhan Reddy <162047840+medamvishnu27@users.noreply.github.com>
2026-03-11 12:48:07 +0530

    Update upcoming batch date to 23rd March 2026

--- src/app/[slug]/courseDetailsPage/upcoming-batch/page.jsx
+++ src/app/[slug]/courseDetailsPage/upcoming-batch/page.jsx
@@ -67,7 +67,7 @@ export default function Page() {
                   <div>
                     <div className="text-3xl font-bold text-blue-600 mb-2">
 
-                      2nd March 2026
+                      23nd March 2026
 
 
                       
