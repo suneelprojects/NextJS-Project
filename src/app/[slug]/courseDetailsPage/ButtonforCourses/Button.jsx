23301ca055bad55fcd1e8e33c3df36da06723318
Vishnu Vardhan Reddy <162047840+medamvishnu27@users.noreply.github.com>
2026-09-22 14:55:05 +0530

    render course lead form through document portal

--- src/app/[slug]/courseDetailsPage/ButtonforCourses/Button.jsx
+++ src/app/[slug]/courseDetailsPage/ButtonforCourses/Button.jsx
@@ -1,29 +1,42 @@
-"use client"
-import React, { useState } from 'react';
-import Buttonstyle from './Button.module.css';
-import SignInForm from '@/components/Forms/coursesForm';
+/** @format */
+"use client";
+
+import React, { useState } from "react";
+import { createPortal } from "react-dom";
+import Buttonstyle from "./Button.module.css";
+import SignInForm from "@/components/Forms/coursesForm";
 
 const Button = ({ label, courseID, actionType }) => {
   const [isPopupVisible, setIsPopupVisible] = useState(false);
 
-  const togglePopup = () => {
-    setIsPopupVisible(!isPopupVisible);
+  const openPopup = () => {
+    setIsPopupVisible(true);
+  };
+
+  const closePopup = () => {
+    setIsPopupVisible(false);
   };
 
   return (
     <div className={Buttonstyle.Enrollbutton}>
-      <button className={Buttonstyle.shinebtn} onClick={togglePopup}>
-        {/* USE CSS MODULE class reference */}
+      <button
+        type="button"
+        className={Buttonstyle.shinebtn}
+        onClick={openPopup}
+      >
         <span className={Buttonstyle.btntext}>{label}</span>
       </button>
 
-      {isPopupVisible && (
-        <SignInForm
-          onClose={togglePopup}
-          courseID={courseID}
-          actionType={actionType}
-        />
-      )}
+      {isPopupVisible &&
+        typeof document !== "undefined" &&
+        createPortal(
+          <SignInForm
+            onClose={closePopup}
+            courseID={courseID}
+            actionType={actionType}
+          />,
+          document.body
+        )}
     </div>
   );
 };
